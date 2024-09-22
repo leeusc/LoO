3faf10d41bbd68305c016daa4b3c7d4a18c9cbd9
leeusc <164704998+leeusc@users.noreply.github.com>
2024-09-22 13:31:21 +0700

    Delete Sistem Informasi Akademik/src directory

--- Sistem Informasi Akademik/src/model/presensi/Status.java
+++
@@ -1,8 +0,0 @@
-package model.presensi;
-
-// create interface, however enum has better improvement compare to interface using Constant
-public interface Status {
-
-    int Hadir = 1;
-    int Absen = 0;
-}
